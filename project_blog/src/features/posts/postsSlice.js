0adc971ddd90097363501fa106a795285ff894e0
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2023-09-25 20:17:53 +0530

    added emojis

--- project_blog/src/features/posts/postsSlice.js
+++ project_blog/src/features/posts/postsSlice.js
@@ -6,45 +6,69 @@ const initialState = [
         id: '1',
         title: 'Learning Redux Toolkit',
         content: "I've heard good things.",
-        date: sub(new Date(), {minutes: 10}).toISOString()
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+        }
     },
-        
     {
         id: '2',
         title: 'Slices...',
         content: "The more I say slice, the more I want pizza.",
-        date: sub(new Date(), {minutes: 5}).toISOString()
-
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+        }
     }
 ]
 
 const postsSlice = createSlice({
-    name: 'posts', 
-    initialState, 
+    name: 'posts',
+    initialState,
     reducers: {
-        postAdded : {
-            reducer(state, action){
-                state.push(action.payload);
+        postAdded: {
+            reducer(state, action) {
+                state.push(action.payload)
             },
-            prepare(title, content, userId){ // prepare is used for defining the action payload in a more organized and consistent way. 
-                return{
+            prepare(title, content, userId) {
+                return {
                     payload: {
                         id: nanoid(),
-                        title, 
+                        title,
                         content,
                         date: new Date().toISOString(),
-                        userId
+                        userId,
+                        reactions: {
+                            thumbsUp: 0,
+                            wow: 0,
+                            heart: 0,
+                            rocket: 0,
+                            coffee: 0
+                        }
                     }
                 }
             }
-            
-    }
+        },
+        reactionAdded(state, action) {
+            const { postId, reaction } = action.payload
+            const existingPost = state.find(post => post.id === postId)
+            if (existingPost) {
+                existingPost.reactions[reaction]++
+            }
+        }
     }
 })
 
 export const selectAllPosts = (state) => state.posts;
 
-export const {postAdded} = postsSlice.actions
+export const { postAdded, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
-        
