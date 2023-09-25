fbd6f5dda60d63b41373b695c87ec6c4f1966507
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2023-09-25 18:18:48 +0530

    form added

--- project_blog/src/features/posts/postsSlice.js
+++ project_blog/src/features/posts/postsSlice.js
@@ -18,11 +18,15 @@ const postsSlice = createSlice({
     name: 'posts', 
     initialState, 
     reducers: {
-
+        postAdded(state, action){
+            state.push(action.payload);
+        }
     }
 })
 
 export const selectAllPosts = (state) => state.posts;
+
+export const {postAdded} = postsSlice.actions
 
 export default postsSlice.reducer
         
